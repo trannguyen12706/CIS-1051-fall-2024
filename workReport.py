e7324ec493ab6584a09b033608ba0babbdcd37fc
trannguyen12706 <trannguyen12706@gmail.com>
2024-12-09 17:20:55 -0500

    Create workReport.py

---
+++ workReport.py
@@ -0,0 +1,11 @@
+
+I didn't add sound effects to my video game.
+
+
+I added the jump setting for my character but didn't add an
+obstacle for my character to jump over yet(the lava hole
+I mentioned in the proposal)
+
+
+Although my character can jump it can't jump while i hold down
+the left or right keyboard; can only do it separately, not at the same time.
